ad9b620b74b869aaa7204088310f021867867b12
関谷丈二 <joji.sekiya@gmail.com>
2017-04-12 15:34:56 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: "tweets#index"
+  resources :tweets, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
+  resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
+end
